623749f300f7c8e63ffe87e1b0fa58e2201cd043
hanaa2004 <113058547+hanaa2004@users.noreply.github.com>
2024-09-02 15:27:42 +0300

    Add files via upload

---
+++ webapiconsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Json;
+
+namespace webapiconsole
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            HttpClient httpClient = new HttpClient();
+            dept dept= new dept();
+            Console.WriteLine("enter dept name ");
+            dept.name = Console.ReadLine();
+
+            Console.WriteLine("enter manager name ");
+            dept.managername = Console.ReadLine();
+
+            await httpClient.PostAsJsonAsync<dept>("https://localhost:7180/api/dept", dept);
+           
+                Console.WriteLine("enter dept id");
+                int id = int.Parse(Console.ReadLine());
+
+                dept s = await httpClient.GetFromJsonAsync<dept>("https://localhost:7180/api/dept/" + id);
+
+                //Console.WriteLine(s.id);
+                Console.WriteLine(s.name);
+                Console.WriteLine(s.managername);
+
+            
+
+
+        }
+    }
+}
---
+++ webapiconsole/dept.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webapiconsole
+{
+    internal class dept
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string managername { get; set; }
+    }
+}
